56a29e81d3dcaaf803e5e7bfd73454868ef5b31b
NotchArrow <149845992+NotchArrow@users.noreply.github.com>
2025-06-12 15:46:39 -0400

    Full Functionality

--- src/main/java/com/notcharrow/dolphinessence/DolphinEssence.java
+++ src/main/java/com/notcharrow/dolphinessence/DolphinEssence.java
@@ -2,23 +2,24 @@ package com.notcharrow.dolphinessence;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DolphinEssence implements ModInitializer {
-	public static final String MOD_ID = "dolphin-essence";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ServerTickEvents.END_SERVER_TICK.register(server -> {
+			for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+				if (player.hasStatusEffect(StatusEffects.CONDUIT_POWER)) {
+					player.addStatusEffect(
+							new StatusEffectInstance(StatusEffects.DOLPHINS_GRACE, 1, 0,
+									true, true, true));
+				}
+			}
+		});
 	}
 }
